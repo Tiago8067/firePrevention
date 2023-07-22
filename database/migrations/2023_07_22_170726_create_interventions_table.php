91a9eaf9a2573690debd23b390340521640799c5
Filipe Barros <tiagosoarez6@gmail.com>
2023-07-22 21:58:18 +0100

    inicio da intervatiopns, criacao da bd para ela visto que ainda nao se sabe se estara completa

--- database/migrations/2023_07_22_170726_create_interventions_table.php
+++ database/migrations/2023_07_22_170726_create_interventions_table.php
@@ -13,6 +13,47 @@ return new class extends Migration
     {
         Schema::create('interventions', function (Blueprint $table) {
             $table->id();
+            /* $table->string('nome_comercial'); */
+            $table->string('nome_cliente');
+            $table->date('data_servico');
+            $table->string('viatura_ou_loja');
+            /* $table->string('tecnico'); */
+            $table->text('observacao');
+            $table->integer('nr_interno');
+            $table->text('nr_serie');
+            $table->float('capacidade_kg');
+            /* $table->boolean('persao_permanente', 10, 3); */
+            $table->boolean('persao_permanente');
+            $table->string('nome_fabricante');
+            $table->year('ano_fabrico');
+            $table->boolean('marcacao_CE')->default(true);
+            $table->text('localizacao');
+            $table->date('data_ultimo_carregamento');
+            $table->date('data_ultima_prova_hidraulica');
+            $table->boolean('manutencao_MNT');
+            $table->boolean('carregamento_MNT_AD');
+            // $table->string('tipo');
+            // $table->float('peso_CO2_kg', 10, 3);
+            $table->float('peso_CO2_kg');
+            $table->boolean('prova_hidraulica');
+            $table->boolean('selo_seguranca');
+            $table->boolean('O_Ring');
+            $table->boolean('cavilha');
+            $table->boolean('manometro');
+            $table->boolean('difusor');
+            $table->boolean('base_plastica');
+            $table->boolean('rotulo');
+            $table->boolean('sparklets_CO2');
+            $table->boolean('aprovado');
+            $table->string('serv_carga');
+            $table->boolean('extintor_novo');
+            $table->text('motivo_rejeitado');
+            /**
+             * Associar o Artigo do Programada de Faturação
+             * Exportar para PDF
+             * Enviar por Email
+             * Faturado Sim ou Não
+             */
             $table->timestamps();
         });
     }
