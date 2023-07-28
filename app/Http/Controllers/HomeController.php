08c8bfeab2490485a82b17a6316dcba64c625f3a
Filipe Barros <tiagosoarez6@gmail.com>
2023-07-28 08:12:05 +0100

    correcao de graficos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,12 +12,15 @@ class HomeController extends Controller
     public function index()
     {
         $chart_options = [
-            'chart_title' => 'Users by months',
-            'report_type' => 'group_by_date',
-            'model' => 'App\Models\User',
-            'group_by_field' => 'created_at',
-            'group_by_period' => 'month',
+            'chart_title' => 'Intervenções por Funcionário',
             'chart_type' => 'bar',
+            'report_type' => 'group_by_relationship',
+            'model' => 'App\Models\Intervention',
+
+            'relationship_name' => 'user',
+            'group_by_field' => 'name',
+            /* 'group_by_field' => 'created_at',
+            'group_by_period' => 'month', */
         ];
 
         $chart1 = new LaravelChart($chart_options);
--- resources/views/homePage/home.blade.php
+++ resources/views/homePage/home.blade.php
@@ -8,14 +8,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Dashboard</div>
+                    <div class="card-header">Estatísticas Gerais - Gráficos</div>
 
                     <div class="card-body">
 
-                        {{-- <h1>{{ $chart1->options['chart_title'] }}</h1>
+                        <h1>{{ $chart1->options['chart_title'] }}</h1>
                         {!! $chart1->renderHtml() !!}
 
-                        <hr /> --}}
+                        <hr />
 
                         <h1>{{ $chart2->options['chart_title'] }}</h1>
                         {!! $chart2->renderHtml() !!}
